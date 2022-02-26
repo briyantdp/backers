ecaac89666ced16cba652fe8cbd114b467187fc3
Bryant Dawson Priyantoro <briyantazazel7@gmail.com>
2022-02-27 05:41:44 +0700

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        ignores: [
+          'Navbar',
+          'Footer',
+          'index',
+          'login',
+          'auth',
+          'error',
+          'success',
+          'register',
+          'upload',
+          'create',
+          'transactions',
+          'detail',
+        ],
+      },
+    ],
+  },
+}
---
+++ layouts/error.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container mx-auto h-screen flex justify-center items-center">
+    <div class="w-full lg:w-1/3 px-10 lg:px-0">
+      <div class="flex justify-center items-center mx-auto mt-6 mb-8">
+        <img src="../static/svg/404-illustration.svg" alt class="w-full" />
+      </div>
+      <h2 class="font-medium mb-3 text-3xl text-center">Oops! something wrong</h2>
+      <p v-if="error.statusCode === 404" class="text-center font-light">
+        The page that you requsted doesn’t
+        <br />exist at this moment
+      </p>
+      <p v-else class="text-center font-light">An error occurred</p>
+      <div class="mb-4 mt-6">
+        <div class="mb-3">
+          <button
+            class="block w-full bg-orange-button hover:bg-green-button text-white font-semibold px-6 py-4 text-lg rounded-full"
+            @click="$router.push({ path: '/' })"
+          >Back to Home</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'success',
+  props: {
+    error: {
+      type: Object,
+      default: () => { }
+    }
+  },
+}
+</script>
